dc96eebf4a6e7c38a4e8f4951dd6e025042d83de
Hachour Fouad <hachour5fouad@gmail.com>
2021-02-08 14:20:55 +0100

    Moving to ibeacon

--- app/src/main/java/com/example/MyAppPubNub/Scanner.java
+++ app/src/main/java/com/example/MyAppPubNub/Scanner.java
@@ -52,134 +52,59 @@ public class Scanner extends ScanCallback {
 
         for (int index = 0; index < results.size(); ++index) {
             try {
-                /**
-
-                 counter = counter + 1;
-                 ScanRecord mScanRecord = results.get(j).getScanRecord();
-                 manufacturerData = mScanRecord.getManufacturerSpecificData();
-                 byte[] manufacturerData2 = mScanRecord.getManufacturerSpecificData(224);
-
-                 int mRsi = results.get(j).getRssi();
-
-                 if (manufacturerData.size() > 0) {
-                 for (int i = 0; i < manufacturerData.size(); i++) {
-                 int key = manufacturerData.keyAt(i);
-                 byte[] obj = manufacturerData.get(key);
-                 Log.i("taille", Utils.bytesToHex(obj));
-                 byte[] uuid = new byte[16];
-                 byte[] major = new byte[2];
-                 byte[] minor = new byte[2];
-                 byte[] powerX = new byte[1];
-
-                 if (obj[0] == 2 && obj[1] == 21) {
-                 System.arraycopy(obj, 2, uuid, 0, uuid.length);
-                 System.arraycopy(obj, 18, major, 0, major.length);
-                 System.arraycopy(obj, 20, minor, 0, minor.length);
-                 System.arraycopy(obj, 22, powerX, 0, powerX.length);
-                 int powerXValue = Utils.getPower(powerX);
-                 int minorValue = Utils.getMinorOrMajor(minor);
-                 int majorValue = Utils.getMinorOrMajor(major);
-                 double distance = Utils.calculateDistance((int) powerXValue, (double) mRsi);
-                 beaconList.add(new String[]{Utils.bytesToHex(uuid), String.valueOf(majorValue), String.valueOf(minorValue), String.format("%.2f", distance),"ibeacon"});
-                 }
-                 }
-                 //programAdapter = new ProgramAdapter(MainActivity.this, beaconList);
-                 //recyclerView.setAdapter(programAdapter);
-
-                 ProgramAdapter whatever = new ProgramAdapter(MainActivity.this, beaconList);
-                 listView.setAdapter(whatever);
-
-                 }**/
-
                 ScanRecord mScanRecord = results.get(index).getScanRecord();
-                Map<ParcelUuid, byte[]> myMap = mScanRecord.getServiceData();
-                int mRsi = results.get(index).getRssi();
-                String url = "";
-                byte[] txPower = new byte[1];
-                byte[] nameSpaceId = new byte[10];
-                byte[] instanceId = new byte[6];
-                float distance;
+                byte[] iBeaconFrame = mScanRecord.getManufacturerSpecificData(76);
+                if (iBeaconFrame != null && iBeaconFrame.length != 0) {
+                    
+                } else {
+                    Map<ParcelUuid, byte[]> myMap = mScanRecord.getServiceData();
+                    int mRsi = results.get(index).getRssi();
+                    String url = "";
+                    byte[] txPower = new byte[1];
+                    byte[] nameSpaceId = new byte[10];
+                    byte[] instanceId = new byte[6];
+                    float distance;
 
-                /**byte[] myMap = mScanRecord.getServiceData(serviceUid);
+                    for (Map.Entry<ParcelUuid, byte[]> eddystoneFrame : myMap.entrySet()) {
+                        // for eddystone URL
+                        if (eddystoneFrame.getValue()[0] == 16) {
+                            url += urlSchemePrefix[eddystoneFrame.getValue()[2]];
 
-                 if(myMap[0] == 16){
-                 url = url + urlSchemePrefix[myMap[2]];
+                            for (int i = 3; i < eddystoneFrame.getValue().length - 1; i++) {
+                                url += (char) eddystoneFrame.getValue()[i];
+                            }
 
-                 for(int i=3;i<myMap.length-1;i++){
-                 url = url + (char)myMap[i];
-                 }
+                            url += topLevelDomain[eddystoneFrame.getValue()[eddystoneFrame.getValue().length - 1]];
+                            txPower[0] = eddystoneFrame.getValue()[1];
+                            distance = (float) Utils.calculateDistance((int) txPower[0], (double) mRsi);
 
-                 url = url + topLevelDomain[myMap[myMap.length-1]];
-                 txPower[0] = myMap[1];
-                 distance = Utils.calculateDistance((int) txPower[0], (double) mRsi);
+                            try {
+                                beaconManager.addEddyBeacon(Utils.bytesToHex(nameSpaceId), Utils.bytesToHex(instanceId), distance);
+                            } catch (Exception e) {
+                                Log.e(TAG, e.toString());
+                            }
 
-                 beaconList.add(new String[]{"URL" + url,
-                 String.format("%.2f", distance),
-                 "eddystoneurl"});
+                        } else if (eddystoneFrame.getValue()[0] == 0) {
+                            // For Eddystone UID
+                            System.arraycopy(eddystoneFrame.getValue(), 2, nameSpaceId, 0, nameSpaceId.length);
+                            System.arraycopy(eddystoneFrame.getValue(), 12, instanceId, 0, instanceId.length);
+                            System.arraycopy(eddystoneFrame.getValue(), 1, txPower, 0, txPower.length);
 
-                 beaconTitle.setText("EddyStone URL");
-                 majorText.setText(" ");
-                 minorText.setText(" ");
+                            distance = (float) Utils.calculateDistance((int) txPower[0], (double) mRsi);
 
-                 } else if(myMap[0] == 0){
+                            /**  beaconList.add(new String[]{"Name Space ID : " + Utils.bytesToHex(nameSpaceId)+ "\n" + "Instance ID :" + Utils.bytesToHex(instanceId),
+                             String.format("%.2f", distance),
+                             "eddystoneuid"});**/
+                            try {
+                                beaconManager.addEddyBeacon(Utils.bytesToHex(nameSpaceId), Utils.bytesToHex(instanceId), distance);
+                            } catch (Exception e) {
+                                Log.e(TAG, e.toString());
+                            }
 
-                 System.arraycopy(myMap, 2, nameSpaceId, 0, nameSpaceId.length);
-                 System.arraycopy(myMap, 12, instanceId, 0, instanceId.length);
-                 System.arraycopy(myMap, 1, txPower, 0, txPower.length);
-
-                 distance = Utils.calculateDistance((int) txPower[0], (double) mRsi);
-
-                 beaconList.add(new String[]{"Name Space ID : " + Utils.bytesToHex(nameSpaceId)+ "\n" + "Instance ID :" + Utils.bytesToHex(instanceId),
-                 String.format("%.2f", distance),
-                 "eddystoneuid"});
-
-                 beaconTitle.setText("EddyStone UID");
-                 majorText.setText(" ");
-                 minorText.setText(" ");
-                 }**/
-
-                for (Map.Entry<ParcelUuid, byte[]> eddystoneFrame : myMap.entrySet()) {
-                    // for eddystone URL
-                    if (eddystoneFrame.getValue()[0] == 16) {
-                        url += urlSchemePrefix[eddystoneFrame.getValue()[2]];
-
-                        for (int i = 3; i < eddystoneFrame.getValue().length - 1; i++) {
-                            url += (char) eddystoneFrame.getValue()[i];
                         }
-
-                        url += topLevelDomain[eddystoneFrame.getValue()[eddystoneFrame.getValue().length - 1]];
-                        txPower[0] = eddystoneFrame.getValue()[1];
-                        distance = (float) Utils.calculateDistance((int) txPower[0], (double) mRsi);
-
-                        /**beaconList.add(new String[]{"URL" + url,
-                         String.format("%.2f", distance),
-                         "eddystoneurl"});**/
-                        try {
-                            beaconManager.addEddyBeacon(Utils.bytesToHex(nameSpaceId), Utils.bytesToHex(instanceId), distance);
-                        } catch (Exception e) {
-                            Log.e(TAG, e.toString());
-                        }
-
+                        Log.i(TAG, "The size of the frame is: " + eddystoneFrame.getValue().length);
                     }
-                    else if (eddystoneFrame.getValue()[0] == 0) {
-                        // For Eddystone UID
-                        System.arraycopy(eddystoneFrame.getValue(), 2, nameSpaceId, 0, nameSpaceId.length);
-                        System.arraycopy(eddystoneFrame.getValue(), 12, instanceId, 0, instanceId.length);
-                        System.arraycopy(eddystoneFrame.getValue(), 1, txPower, 0, txPower.length);
 
-                        distance = (float) Utils.calculateDistance((int) txPower[0], (double) mRsi);
-
-                        /**  beaconList.add(new String[]{"Name Space ID : " + Utils.bytesToHex(nameSpaceId)+ "\n" + "Instance ID :" + Utils.bytesToHex(instanceId),
-                         String.format("%.2f", distance),
-                         "eddystoneuid"});**/
-                        try {
-                            beaconManager.addEddyBeacon(Utils.bytesToHex(nameSpaceId), Utils.bytesToHex(instanceId), distance);
-                        } catch (Exception e) {
-                            Log.e(TAG, e.toString());
-                        }
-
-                    }
-                    Log.i(TAG, "The size of the frame is: " + eddystoneFrame.getValue().length);
                 }
 
 
